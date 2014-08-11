c5a5b2f511684b4788fc1d5d6e003cc62dca5410
KUMARSHANMUGAM <KUMARSHANMUGAM@dsrc.co.in>
2014-08-11 16:20:57 +0530

    Changes

--- testSE/src/secretEscape/NewTest.java
+++ testSE/src/secretEscape/NewTest.java
@@ -50,7 +50,7 @@ public class NewTest {
 	  driver.manage().window().maximize();
 	  driver.get(baseUrl);
 	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	  
-		  
+	  System.out.print("Test");	  
   }
 
   @AfterMethod
